4978672591010c35e5364c23042d8bad347622ec
Miranti20 <rantirarrr4505@gmail.com>
2025-03-18 10:58:01 +0700

    Perulanagn for untuk pencacah naik

---
+++ file1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int cacah;
+    // perulangan for untuk pencacah naik
+    for(cacah = 0; cacah <= 4; cacah++){
+        cout << "Nilai pencacah = " << " Selamat Datang!" << endl;
+
+    }
+    cout << " Nilai pencacah terakhir = " << cacah;
+    cout << endl;
+    cout << endl;
+
+    
