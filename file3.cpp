7c7c3ce7759271660ce1a49162ee1199fc0e10cc
Miranti20 <rantirarrr4505@gmail.com>
2025-03-18 12:10:03 +0700

    menambahkan switch

---
+++ file3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+double hitungIpk(double total, int sks)
+{
+    return total / sks;
+}
+
+string predikat (double ipk){
+    if (ipk < 10){
+        return "memuaskan";
+    }
+    else{
+        return "cumlaude";
+    }
+}
+
